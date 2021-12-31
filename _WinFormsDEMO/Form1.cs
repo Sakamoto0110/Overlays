afeb662485959e3555232b441ff0e9e9e579cd22
Sakamoto0110 <rafael.sakamoto1@hotmail.com>
2021-12-31 12:54:16 -0300

    Created

---
+++ _WinFormsDEMO/Form1.cs
@@ -0,0 +1,19 @@
+using OverlayLib;
+
+namespace _WinFormsDEMO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            IOverlay overlay = new Overlay(Handle);
+            overlay.Opacity = 128;
+            overlay.IsCaptionEnabled = false;
+            
+            this.Size = new Size(900, 900);
+            overlay.IsTransparent = true;
+            overlay.IsCaptionEnabled = true;
+        }
+    }
+}
